40c07506b4d3de763207e6575895da20ac823b01
Mirka McIntyre <mirka.kiana@gmail.com>
2025-06-02 16:09:33 -0400

    Made arrrays.

---
+++ js/wordloop.js
@@ -0,0 +1,32 @@
+// make array for the word columns
+
+const wC1 = [
+    'Your cat', 
+    'My dog', 
+    'The rabbit',
+    'The chicken',
+    'The cow'];
+
+const wC2 = [
+    'falls',
+    'jumps',
+    'runs',
+    'digs',
+    'poops'];
+
+const wC3 = [
+    'into his',
+    'on your',
+    'between the',
+    'for your',
+    'by the',
+];
+
+const wC4 = [
+    'couch',
+    'food',
+    'cow',
+    'face',
+    'table'
+];
+
