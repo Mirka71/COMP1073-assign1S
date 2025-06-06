90ff40845e3f7cc91ae8f4daa1b8a032024ac212
Mirka McIntyre <mirka.kiana@gmail.com>
2025-06-05 23:46:21 -0400

    Added 3 more global vars and had them start at -1 so they increment to 0. I also fixed an error.

--- js/wordloop.js
+++ js/wordloop.js
@@ -47,7 +47,10 @@ function randomNum () {
 const complete = [];
 
 // Make a global var
-let arrayIndex = 0;
+let arrayIndex = -1;
+let arrayIndex2 = -1;
+let arrayIndex3 = -1;
+let arrayIndex4 = -1;
 
 
 // Created a function to ensure the correct array index variable is selected.
@@ -62,29 +65,29 @@ function increment(value){
     }
 
     else if(value == 1) {
-        if(arrayIndex > 3){
-            arrayIndex = 0;
+        if(arrayIndex2 > 3){
+            arrayIndex2 = 0;
         }else{
-            arrayIndex++
+            arrayIndex2++
         }
-        return arrayIndex
+        return arrayIndex2
     }
     else if(value == 2) {
-        if(arrayIndex > 3) {
-            arrayIndex = 0;
+        if(arrayIndex3 > 3) {
+            arrayIndex3 = 0;
         }else{
-            arrayIndex++
+            arrayIndex3++
         }
-        return arrayIndex
+        return arrayIndex3
     }
 
     else if(value == 3) {
-        if(arrayIndex > 3) {
-            arrayIndex = 0;
+        if(arrayIndex4 > 3) {
+            arrayIndex4 = 0;
         }else{
-            arrayIndex++
+            arrayIndex4++
         }
-        return arrayIndex
+        return arrayIndex4
     }
     return;
 }
@@ -100,10 +103,10 @@ function addValue(order) {
         complete[order] = wC2[increment(1)].toString();
         document.getElementById('word2').innerHTML = complete[order];
     }else if(order == 2){
-        complete[order] = wC2[increment(2)].toString();
+        complete[order] = wC3[increment(2)].toString();
         document.getElementById('word3').innerHTML = complete[order];
     }else if(order == 3){
-        complete[order] = wC3[increment(3)].toString();
+        complete[order] = wC4[increment(3)].toString();
         document.getElementById('word4').innerHTML = complete[order];
     }else{
         return;
